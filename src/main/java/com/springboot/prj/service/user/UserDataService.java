0b8bbfc80b8652abc23751d4eab75aa3fe422161
ducn03 <dinhduclt03@gmail.com>
2025-02-25 01:20:50 +0700

    init

--- src/main/java/com/springboot/prj/service/user/UserDataService.java
+++ src/main/java/com/springboot/prj/service/user/UserDataService.java
@@ -1,8 +1,5 @@
 package com.springboot.prj.service.user;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface UserDataService {
 
 }
--- src/main/java/com/springboot/prj/service/user/UserDataServiceImpl.java
+++ src/main/java/com/springboot/prj/service/user/UserDataServiceImpl.java
@@ -1,4 +1,7 @@
 package com.springboot.prj.service.user;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserDataServiceImpl implements UserDataService{
 }
