6f7301f134c16259f6c9f13dcc99620c36bcb367
ducn03 <dinhduclt03@gmail.com>
2026-01-10 23:46:57 +0700

    chore: refactor - send notify

--- src/main/java/com/springboot/notification/service/notify/NotifyService.java
+++ src/main/java/com/springboot/notification/service/notify/NotifyService.java
@@ -9,17 +9,46 @@ import com.springboot.notification.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutorService;
+
 @Slf4j
 @Service
 public class NotifyService {
     private final MessageSenderFactory senderFactory;
+    private final ExecutorService executorService;
     private final static long BALANCE_WARNING = 100000;
 
-    public NotifyService(MessageSenderFactory senderFactory) {
+    public NotifyService(MessageSenderFactory senderFactory, ExecutorService executorService) {
         this.senderFactory = senderFactory;
+        this.executorService = executorService;
     }
 
     public boolean sendNotify(NotifyRequest notifyRequest) {
+        MessageSender sender = senderFactory.getSender(notifyRequest.getMethod());
+        if (sender == null) {
+            log.error("Unable to obtain an instance of Sender {} : {} : {}",
+                    notifyRequest.getMethod(),
+                    notifyRequest.getUserId(),
+                    notifyRequest.getSendTime());
+            throw new AppException(AppErrorCodes.SYSTEM.BAD_REQUEST);
+        }
+
+        buildContentIfNeed(notifyRequest);
+        boolean result = sender.send(notifyRequest);
+
+        if (result) {
+            executorService.execute(() -> {
+                handleSomethingAfterSendNotification(notifyRequest);
+            });
+        }
+
+        return result;
+    }
+
+    /**
+     * Xử lý những tác vụ này sau khi gửi thông báo
+     */
+    private void handleSomethingAfterSendNotification(NotifyRequest notifyRequest) {
         MessageSender sender = senderFactory.getSender(notifyRequest.getMethod());
         if (sender == null) {
             log.error("Unable to obtain an instance of Sender {} : {} : {}",
@@ -41,9 +70,6 @@ public class NotifyService {
                 && sender.getBalance() <= BALANCE_WARNING) {
             // TODO: Send notify or log v.v..
         }
-
-        buildContentIfNeed(notifyRequest);
-        return sender.send(notifyRequest);
     }
 
     /**
